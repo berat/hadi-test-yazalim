87da9a33877c5353510482a91661675618fa5094
Berat Bozkurt <beratbozkurt1999@gmail.com>
2021-02-01 21:40:56 +0300

    2. yazı serisi için örnek kod eklendi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBox from './SearchBox'
+import Header from './Header'
 
 function App() {
   return (
@@ -8,17 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <SearchBox placeholder="berat" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header title="Berat Bozkurt" desc="Lorem ipsum Createor" image="https://i0.wp.com/filmloverss.com/wp-content/uploads/2019/02/avatar-2-3-filmloverss.jpg?fit=900%2C600&ssl=1" />
       </header>
     </div>
   );
